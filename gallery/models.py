f4f7ca9bad3cfd72dd124be3d041173bfec19078
halima precious <halimaprecious3@gmail.com>
2022-05-29 08:42:06 +0300

    feat: adds location model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    """
+    A class for geographic loactions where a Photo could be taken
+    """
+    city = models.CharField(max_length=244)
+
+    def __str__(self):
+        """
+        String representation
+        """
+        return self.city
