30fe6d84f31422fff3e953cf4b5464af650310f9
halima precious <halimaprecious3@gmail.com>
2022-05-29 08:08:10 +0300

    fix bug

--- mygallery/settings.py
+++ mygallery/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gallery'
-    'bootstrap5'
+    'gallery',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Africa,Nairobi'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
